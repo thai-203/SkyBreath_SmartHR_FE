284bc08acdaddc5a401e6d5d11b932835c738c74
Minhktm02 <minhktm02@gmail.com>
2026-02-03 23:48:50 +0700

    curd employee

--- src/lib/api.js
+++ src/lib/api.js
@@ -3,7 +3,7 @@ import axios from "axios";
 const api = axios.create({
   baseURL: process.env.NEXT_PUBLIC_API_URL || "http://localhost:3000/api/v1",
   headers: {
-    "Content-Type": "application/json",
+    Accept: "application/json",
   },
   withCredentials: true,
 });
