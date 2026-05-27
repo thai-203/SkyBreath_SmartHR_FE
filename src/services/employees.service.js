1535ac80f19905f25a5af35fb294d31bdced265a
thai <damthai2003@gmail.com>
2026-05-28 01:15:44 +0700

    update 28/05/2026

--- src/services/employees.service.js
+++ src/services/employees.service.js
@@ -5,7 +5,7 @@ export const employeesService = {
     const response = await api.get("/employees", { params });
     return response.data;
   },
-  
+
   getAllForPublic: async (params = {}) => {
     const response = await api.get("/employees/public", { params });
     return response.data;
@@ -53,8 +53,8 @@ export const employeesService = {
     return response.data;
   },
 
-  getEmployeeNoPlanId: async () => {
-    const response = await api.get("/employees/no-plan");
+  getEmployeeNoPlanId: async (params = {}) => {
+    const response = await api.get("/employees/no-plan", { params });
     return response.data;
   },
 };
