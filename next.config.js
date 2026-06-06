aee0794d057a30eebf909a4fc6fe889218cab3c1
checkerfpt13 <checkerfpt13@gmail.com>
2026-06-06 08:57:47 +0700

    ok

--- next.config.js
+++ next.config.js
@@ -24,7 +24,7 @@ const nextConfig = {
         headers: [
           {
             key: "Content-Security-Policy",
-            value: "default-src 'self'; script-src 'self' 'unsafe-eval' 'unsafe-inline' https://cdnjs.cloudflare.com https://cdn.jsdelivr.net; style-src 'self' 'unsafe-inline' https://cdnjs.cloudflare.com https://fonts.googleapis.com; img-src 'self' data: blob: https://*.openstreetmap.org https://cdnjs.cloudflare.com http://localhost:* https://localhost:* https://images.unsplash.com https://ui-avatars.com https://placehold.co https://res.cloudinary.com; font-src 'self' data: https://fonts.gstatic.com; connect-src 'self' data: http://localhost:* ws://localhost:* https://*.openstreetmap.org https://cdn.jsdelivr.net https://api.ipify.org; frame-src 'self'; object-src 'none';",
+            value: "default-src 'self'; script-src 'self' 'unsafe-eval' 'unsafe-inline' https://cdnjs.cloudflare.com https://cdn.jsdelivr.net; style-src 'self' 'unsafe-inline' https://cdnjs.cloudflare.com https://fonts.googleapis.com; img-src 'self' data: blob: https://*.openstreetmap.org https://cdnjs.cloudflare.com http://localhost:* https://localhost:* https://images.unsplash.com https://ui-avatars.com https://placehold.co https://res.cloudinary.com https://*.skybreath-hrm.cloud https://skybreath-hrm.cloud; font-src 'self' data: https://fonts.gstatic.com; connect-src 'self' data: http://localhost:* ws://localhost:* https://*.openstreetmap.org https://cdn.jsdelivr.net https://api.ipify.org https://*.skybreath-hrm.cloud wss://*.skybreath-hrm.cloud https://skybreath-hrm.cloud; frame-src 'self'; object-src 'none';",
           },
           {
             key: "X-Frame-Options",
